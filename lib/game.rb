848f6682838c2e8ca65a0a7adb52728097b8518f
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2021-07-20 11:58:06 +0100

    Add other rules with tests

--- lib/game.rb
+++ lib/game.rb
@@ -7,6 +7,14 @@ class Game
   end
 
   def play
-    "draw" if player1 == player2
+    return "draw" if player1 == player2
+
+    return "player1 wins" if player1 == "rock" && player2 == "scissors"
+    return "player1 wins" if player1 == "scissors" && player2 == "paper"
+    return "player1 wins" if player1 == "paper" && player2 == "rock"
+
+    return "player2 wins" if player2 == "rock" && player1 == "scissors"
+    return "player2 wins" if player2 == "scissors" && player1 == "paper"
+    return "player2 wins" if player2 == "paper" && player1 == "rock"
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,4 +5,32 @@ RSpec.describe Game do
   it "returns draw if player 1 and player 2 select the same thing" do
     expect(described_class.new("rock", "rock").play).to eq("draw")
   end
+
+  context "player1 wins" do
+    it "player1 has rock and player2 scissors" do
+      expect(described_class.new("rock", "scissors").play).to eq("player1 wins")
+    end
+
+    it "player1 has scissors and player2 paper" do
+      expect(described_class.new("scissors", "paper").play).to eq("player1 wins")
+    end
+
+    it "player1 has paper and player2 rock" do
+      expect(described_class.new("paper", "rock").play).to eq("player1 wins")
+    end
+  end
+
+  context "player2 wins" do
+    it "player2 has rock and player1 scissors" do
+      expect(described_class.new("scissors", "rock").play).to eq("player2 wins")
+    end
+
+    it "player2 has scissors and player1 paper" do
+      expect(described_class.new("paper", "scissors").play).to eq("player2 wins")
+    end
+
+    it "player2 has paper and player1 rock" do
+      expect(described_class.new("rock", "paper").play).to eq("player2 wins")
+    end
+  end
 end
